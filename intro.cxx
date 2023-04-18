e8bf35042a784af530283d97b2e7b982d8e617b0
Vinpush <130713121+Vinpush@users.noreply.github.com>
2023-04-18 08:35:07 +0530

    Add files via upload

---
+++ intro.cxx
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char name[1024];
+	printf("What is your name ?\n");
+	scanf("%s", name);
+	printf(" what is your father's name?\n");
+	scanf("%s", name);
+	printf(" what is your mother's name?\n");
+	scanf("%s", name);
+	printf(" which course are you pursuing?\n");
+	scanf("%s", name);
+	printf(" nice to meet you!\n");
+	return 0;
+}
